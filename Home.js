5ab59d602d7147adb25390d823a6bbdc65c9f1e5
Devi Priya S <114299272+DeviPriyasoundararaj@users.noreply.github.com>
2024-06-08 10:22:05 +0530

    Create Home.js

---
+++ Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import UrlShortener from '../components/UrlShortener';
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Welcome to URL Shortener</h2>
+      <UrlShortener />
+    </div>
+  );
+};
+
+export default Home;
